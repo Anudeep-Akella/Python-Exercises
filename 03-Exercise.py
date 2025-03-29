b60f2fe0a4b350fc0007fad650062dc221d7bb62
Anudeep-Akella <anudeepakella@gmail.com>
2025-03-30 01:56:41 +0530

    Execise 3

--- 03-Exercise.py
+++ 03-Exercise.py
@@ -7,18 +7,18 @@ Then, the output should be:
 {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
 '''
 
-def func(x):					#This function calculates the key and value pairs for updating the dictionary
-	'''
-	This is a function
-	'''
-	result = {}
-	if x == 0:
-		return 1
-	for i in range(1,x+1):
-		result.update({i:i*i})
-	return result
+
+def func(x):  # This function calculates the key and value pairs for updating the dictionary
+    '''
+    This is a function
+    '''
+    result = {}
+    if x == 0:
+        return 1
+    for i in range(1, x+1):
+        result.update({i: i*i})
+    return result
+
 
 dic = int(input("Enter the number for updating the Dictionary: "))
 print(f'The dictionary with the keys and values upto {dic} is = {func(dic)}')
-
-
